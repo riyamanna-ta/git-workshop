61caf0cb7ff3636f59c366e70211fb4bb3eb4902
shaileshhariharan-ta <shailesh.hariharan@tigeranalytics.com>
2024-11-05 07:00:49 +0000

    DOC: Defined DocStrings

--- simple_functions.py
+++ simple_functions.py
@@ -1,11 +1,47 @@
 # Custom python functions
 
+
 def double_number(a):
-    print(f'value before double_number(): {a}')
-    result = a+a
-    print(f'value after double_number(): {result}')
+    """
+    Doubles a number
+
+    Parameters
+    ----------
+    a : float
+
+    Returns
+    -------
+    float
+        The double of the number
+
+    Example
+    -------
+    >> double_number(2)
+        4
+    """
+    print(f"value before double_number(): {a}")
+    result = a + a
+    print(f"value after double_number(): {result}")
+
 
 def square_number(a):
-    print(f'value before square_number(): {a}')
-    result = a*a
-    print(f'value after square_number(): {result}')
+    """
+    Squares a number
+
+    Parameters
+    ----------
+    a : float
+
+    Returns
+    -------
+    float
+        The Square of the number
+    Example
+    -------
+    >> square_number(3)
+        9
+    """
+    print(f"value before square_number(): {a}")
+    result = a * a
+    print(f"value after square_number(): {result}")
+    return a + a
